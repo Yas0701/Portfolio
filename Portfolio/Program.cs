126288cf07312ad05b63df30fd4d848be19b4869
Yas0701 <Yasser@NUC-A01>
2026-01-13 14:08:21 +0100

    update van mijn portfolio.

--- Portfolio/Program.cs
+++ Portfolio/Program.cs
@@ -1,4 +1,4 @@
-using Portfolio.Components;
+﻿using Portfolio.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,7 +15,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
 
 app.UseAntiforgery();
